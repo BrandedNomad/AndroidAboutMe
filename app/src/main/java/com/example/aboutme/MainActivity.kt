63e7e8b807d7d1911425ea310bb100617cc9c722
BrandedNomad <info@brandednomad.com>
2022-02-14 14:11:33 +0700

    feat: added nickname function

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -2,11 +2,33 @@ package com.example.aboutme
 
 import androidx.appcompat.app.AppCompatActivity
 import android.os.Bundle
+import android.view.View
+import android.widget.Button
+import android.widget.EditText
+import android.widget.TextView
 
 class MainActivity : AppCompatActivity() {
+
+    lateinit var doneButton:Button
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        doneButton = findViewById(R.id.done_button)
+        doneButton.setOnClickListener{
+            addNickname(it)
+        }
+    }
+
+    private fun addNickname(view: View){
+        val editText:EditText = findViewById(R.id.nickname_edit)
+        val nickNameTextView: TextView = findViewById(R.id.nickname_text)
+
+        nickNameTextView.text = editText.text
+        editText.visibility = View.GONE
+        view.visibility = View.GONE
+        nickNameTextView.visibility = View.VISIBLE
+
     }
 }
