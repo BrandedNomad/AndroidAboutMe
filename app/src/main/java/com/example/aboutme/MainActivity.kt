64284916effbbb0d796667dfbefb886cf9b75df7
BrandedNomad <info@brandednomad.com>
2022-02-15 12:44:13 +0700

    refactor: added dataclass and databinding

--- app/src/main/java/com/example/aboutme/MainActivity.kt
+++ app/src/main/java/com/example/aboutme/MainActivity.kt
@@ -6,29 +6,38 @@ import android.view.View
 import android.widget.Button
 import android.widget.EditText
 import android.widget.TextView
+import androidx.databinding.DataBindingUtil
+import com.example.aboutme.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
 
-    lateinit var doneButton:Button
+    private lateinit var binding: ActivityMainBinding
+
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
+        //setContentView(R.layout.activity_main)
+        binding = DataBindingUtil.setContentView(this,R.layout.activity_main)
 
-        doneButton = findViewById(R.id.done_button)
-        doneButton.setOnClickListener{
+
+        binding.doneButton.setOnClickListener{
             addNickname(it)
         }
     }
 
     private fun addNickname(view: View){
-        val editText:EditText = findViewById(R.id.nickname_edit)
-        val nickNameTextView: TextView = findViewById(R.id.nickname_text)
+        //val editText:EditText = findViewById(R.id.nickname_edit)
+        //val nickNameTextView: TextView = findViewById(R.id.nickname_text)
 
-        nickNameTextView.text = editText.text
-        editText.visibility = View.GONE
-        view.visibility = View.GONE
-        nickNameTextView.visibility = View.VISIBLE
+        binding.apply{
+            nicknameText.text = binding.nicknameEdit.text
+            invalidateAll() //used to refresh the ui with new data
+            nicknameEdit.visibility = View.GONE
+            view.visibility = View.GONE
+            nicknameText.visibility = View.VISIBLE
+        }
+
+
 
     }
 }
